47d4a9e7258dd9334ff614ee6b47aede6d26f766
mcarrollnow <dreamkidmedia@gmail.com>
2025-05-21 08:38:43 -0400

    Initial commit: project structure and base code

--- pages/api/get-products.js
+++
@@ -1,6 +0,0 @@
-// api/get-products.js
-// API NO LONGER USED: All product data is now sourced from pages/index.js only.
-
-module.exports = (req, res) => {
-  return res.status(410).json({ error: 'This API is deprecated. All product data is now sourced from pages/index.js.' });
-};
